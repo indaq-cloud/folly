f39791e7b3a1c225355f63d8460f58c92b4e9ae3
Andres Suarez <asuarez@fb.com>
2021-01-09 19:21:20 -0800

    Apply clang-format update fixes
    Reviewed By: igorsugak
    
    Differential Revision: D25849195
    
    fbshipit-source-id: 611df723a38b5c2a7ba5184c63fdeeed1ed27025

--- folly/synchronization/AtomicNotification-inl.h
+++ folly/synchronization/AtomicNotification-inl.h
@@ -61,7 +61,8 @@ void atomic_wait_impl(const Atom<Integer, Args...>* atomic, Integer expected) {
 }
 
 template <
-    template <typename...> class Atom,
+    template <typename...>
+    class Atom,
     typename... Args,
     typename Clock,
     typename Duration>
@@ -73,7 +74,8 @@ std::cv_status atomic_wait_until_impl(
 }
 
 template <
-    template <typename...> class Atom,
+    template <typename...>
+    class Atom,
     typename Integer,
     typename... Args,
     typename Clock,
